2910ae7082d71628065afedc022cb86597ef28d1
chntlg <chantalguerrero04@gmail.com>
2024-09-09 19:14:52 +0800

    Initial commit with numbers.js

---
+++ numbers.js
@@ -0,0 +1,15 @@
+const firstFavNum = 10;
+const secondFavNum = 4;
+const sum = firstFavNum + secondFavNum;
+const difference =firstFavNum - secondFavNum;
+const product = firstFavNum * secondFavNum;
+const quotient  = firstFavNum / secondFavNum;
+const modulus = firstFavNum % secondFavNum;
+const exponent = firstFavNum **secondFavNum;
+
+console.log("Addition:" , sum);
+console.log("Subtraction:" , difference);
+console.log("Multiplication:" , product);
+console.log("Division:" , quotient);
+console.log("Modulus:" , modulus);
+console.log("Exponentiation:" , exponent);
